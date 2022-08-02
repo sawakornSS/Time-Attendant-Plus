8cffd5830fe3bcafe8237696135bfef0cbfdc47c
sawakornSS <83001203+sawakornSS@users.noreply.github.com>
2022-08-02 09:54:12 +0700

    User-profile Screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,18 @@ import { HomeComponent } from './home/home.component';
 import { WidgetComponent } from './widget/widget.component';
 import { ApproveComponent } from './approve/approve.component';
 import { RequestManagementComponent } from './request-management/request-management.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ReportEmployeeComponent } from './report-employee/report-employee.component';
+import { ImportTimeAttendantComponent } from './import-time-attendant/import-time-attendant.component';
 
 const routes: Routes = [
   {path: "", pathMatch: "full", redirectTo: "/home"},
   {path: "home", component: HomeComponent},
   {path: "request-management", component: RequestManagementComponent},
-  {path: "approve", component:ApproveComponent}
+  {path: "approve", component:ApproveComponent},
+  {path: "reportEmployee", component:ReportEmployeeComponent},
+  {path: "importTime", component:ImportTimeAttendantComponent},
+  {path: "profile", component:UserProfileComponent}
 ];
 
 @NgModule({
